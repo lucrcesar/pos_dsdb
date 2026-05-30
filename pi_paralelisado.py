7462b4427f4638155bfbb64d8d52a207d9152fc6
Janis Boiko <66870291+JanisBoiko1@users.noreply.github.com>
2026-05-30 18:39:57 -0300

    melhorado

--- pi_paralelisado.py
+++ pi_paralelisado.py
@@ -1,6 +1,6 @@
 from multiprocessing import Process
 import time
-### n=100.00.000
+
 
 def pi_naive(start, end, step):
     #marca o inicio
@@ -11,43 +11,61 @@ def pi_naive(start, end, step):
     sum = 0.0
     #para cada retangulo
     for i in range(start, end):
-        #calcula a altura do retangulo pelo paço, que é também a largura
+        #marca onde calcularemos a altura do retangulo pelo paço -- que é também a largura
         x = (i+0.5) * step
-        #atualiza isso na soma (o  que esse 4 faz?)
-        sum = sum + 4.0/(1.0+x*x)
+        #calcula a área do retangulo e atualiza isso na soma
+        #Área = altura (sum+4/1+x*x) *Base (step)
+        #formula da altura vem da derivada da arctangente
+        #4/(1+x*x)
+        #por que 4? por que 1? por que x^2?
+        sum = sum + (4.0/(1.0+x*x)) * step
+        
     print ("Valor Pi: %.10f" %sum)
     return sum
 
 if __name__ == '__main__':
     procs = []
     #haverão 100 milhões de passos, ou seja, 100 milhões de retangulos
-    num_steps = 100 #100.000.000 (10+e8) = 17.1 seg.
+    num_steps = 100_000_000 #100.000.000
+
+    #variável do processo e do início em 4 e 0
     n_process = 4
     inicio = 0
-    for i in range(n_process):
-        tamanho_processo = num_steps/n_process
-        inicio = int( i * (tamanho_processo))
-        fim = int((tamanho_processo * (i+1))-1)
 
+    #loop dos processos, de 0 à 3
+    for i in range(n_process):
+        
+        #tamanho do processo é o número de passos dividido pelo número de processos, ou seja, 25 milhões
+        tamanho_processo = num_steps/n_process
+
+        #inicio é o valor do processo * o tamanho do processo,
+        #Ex 0*25 000 000 = 0; 1*25 000 000 = 25 000 000
+        inicio = int( i * (tamanho_processo))
+
+        #fim = tamanho do processo * passo + 1, -1
+        #Ex (25 000 000 * 0+1) - 1 = 24 999 999
+        fim = int((tamanho_processo * (i+1)) - 1)
+
+        #paço atualizado. 1/X milhões
         step = 1.0/num_steps
         
-        tic = time.time() # Tempo Inicial
+        #tempo inicial
+        tic = time.time()
         
+        #chama o processo p e passa inicio, fim e step
         p = Process(target = pi_naive, args = (inicio, fim, step, ))
+
+        #anexa o processo na lista de processos
         procs.append(p)
+        #inicia
         procs[i].start()
 
-        toc = time.time() # Tempo Final
-        #pi vai ser 1/numero de passos * essa soma local
-        #prints
-        print ("Tempo Pi: %.8f s" %(toc-tic))
+        #tempo final
+        toc = time.time()
+
+        print("Tempo Pi: %.8f s" %(toc-tic))
     
-    #print('hello, sou', 'bob pai')
-    
-    ##TEM QUE SER SEPARADO PORQUE SENÃO PIORA O CÓDIGO
-    ##VIRA SEQUENCIAL COM O CUSTO DO PARALELISMO
-   # for i in range():
-   #     procs[i].start()
+    #loop de 'pagamento dos pedreiros'
     for i in range(n_process):
         procs[i].join()
-   # print('hello, sou bob pai, soma=', soma)
+   #falow valeu
